6114c5c7cd667f8d6389e934d7be2894b5fc73c7
Alexanderdp17 <0200721054@udh.edu.pe>
2022-11-08 17:24:40 -0500

    Diseño de la pagina WELCOME

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Bienvenido alumno') }}
                 </div>
             </div>
         </div>
