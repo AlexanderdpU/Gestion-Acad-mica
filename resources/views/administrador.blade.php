26ac13b1d36d2b1b188fc7897025629cd33c1798
Alexanderdp17 <0200721054@udh.edu.pe>
2022-11-09 20:36:42 -0500

    Diseño tabla administrador

--- resources/views/administrador.blade.php
+++ resources/views/administrador.blade.php
@@ -12,25 +12,80 @@
         <h1>Administrador</h1>
         <nav>
             <ul>
+                <li><a href="#">Welcome</a></li>
                 <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
             </ul>
         </nav>
     </header>
     <section>
-        <hgroup>
-            <h1>Main Section</h1>
-            <h2>This is a sample HTML5 Page</h2>
-        </hgroup>
-        <article>
-            <p>Content of the first article</p>
-        </article>
-        <article>
-            <p>Content of the second article</p>
-        </article>
+        <div class="row">
+            <!-- Datos alumno -->
+            <div class="col-md-3">
+                <ul class="list-group">
+                    <li class="list-group-item">Administrador:</li>
+                    <li class="list-group-item">Pedro Salazar</li>
+                </ul>
+            </div>
+            <!-- Table -->
+            <div class="col-md-9">
+                <table class="table table-hover">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Curso</th>
+                        <th scope="col">Semestre</th>
+                        <th scope="col">Docente</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Redes y Telecomunicaciones I</td>
+                        <td>V</td>
+                        <td>
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected>Seleccione docente</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Calculo I</td>
+                        <td>VI</td>
+                        <td>
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected>Seleccione docente</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Lenguaje de Programacion</td>
+                        <td>VIII</td>
+                        <td>
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected>Seleccione docente</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </td>
+                    </tr>
+            </div>
+            </tbody>
+            </table>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button type="button" class="btn btn-outline-primary">Guardar</button>
+            </div>
     </section>
     <footer>
-        <p>This is the Footer of the web document</p>
+        <p>Universidad de investigación y desarrollo</p>
     </footer>
 </div>
 </body>
